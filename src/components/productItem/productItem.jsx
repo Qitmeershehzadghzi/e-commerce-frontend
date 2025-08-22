5c61d3ffab14df0da17ea807d39901a59a29ffd3
Qetmeer <qitmeershehzad@gmail.com>
2025-08-22 16:47:23 +0500

    updated code

--- src/components/productItem/productItem.jsx
+++ src/components/productItem/productItem.jsx
@@ -7,12 +7,14 @@ function ProductItem({ id, name, image, price }) {
   const { currency } = useContext(ShopContext);
 
   return (
-    <div>
-      <Link to={`/product/${id}`} className='product-item'>
-        <div className='product-image'>
-          <img src={image} alt={name} className='image' />
-          <p className='product-name'>{name}</p>
-          <p className='product-details'>
+    <div className="product-card">
+      <Link to={`/product/${id}`} className="product-item">
+        <div className="product-image">
+          <img src={image[0]} alt={name} className="image" />
+        </div>
+        <div className="product-info">
+          <p className="product-name">{name}</p>
+          <p className="product-details">
             {currency} {price}
           </p>
         </div>
