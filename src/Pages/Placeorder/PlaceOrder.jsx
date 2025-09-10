39c460e043705241a3871decb2673b5aa8ed47ad
Qetmeer <qitmeershehzad@gmail.com>
2025-09-10 20:08:51 +0500

    code updated

--- src/Pages/Placeorder/PlaceOrder.jsx
+++ src/Pages/Placeorder/PlaceOrder.jsx
@@ -4,32 +4,54 @@ import CartTotal from '../../components/CartTotal/CartTotal'
 import { assets } from '../../assets/frontend_assets/assets'
 import './Placeorder.css';
 import { ShopContext } from '../../context/ShopContext'
+import { data } from 'react-router-dom';
 
 function PlaceOrder() {
-  const {navigate}=useContext(ShopContext)
+  const {navigate,backendUrl,token,cartItems,setCartitems,getCardAmount,delivery_fee,products}=useContext(ShopContext)
   const [method,setMethod]=useState('cod')
+  const[formData,setFormData]=useState({
+      firstName:'',
+      lastName:'',
+      email:'',
+      street:'',
+      City:'',
+      State:'',
+      zipcode:'',
+      country:'',
+      phone:''
+  })
+  const onChangeHandler =(event)=>{
+    const name =event.target.name
+    const value =event.target.value
+
+
+    setFormData(data =>({...data,[name]:value}))
+  }
+  const onSubmitHandler =(e)=>{
+    e.preventDefault()
+  }
   return (
-    <div className='div-1'>
+    <from onSubmit={onSubmitHandler} className='div-1'>
      {/* Left side */}
       <div className='div-2'>
 <div className='div-3'>
 <Title text1={'DELIVERY'} text2={'INFORMATION'} />
 </div>
 <div className='div-4'>
-<input type="text" placeholder="FIRST NAME" className="inp" />
-<input type="text" placeholder="LAST NAME" className="inp-1" />
+<input required onChange={onChangeHandler} name='firstName' value={formData.firstName} type="text" placeholder="FIRST NAME" className="inp" />
+<input required onChange={onChangeHandler} name='lastName' value={formData.lastName} type="text" placeholder="LAST NAME" className="inp-1" />
 </div>
-<input type="email" placeholder="EMAIL ADDRESS" className="inp-2" />
-<input type="email" placeholder="STREET" className="inp-3" />
+<input required onChange={onChangeHandler} name='email' value={formData.email} type="email" placeholder="EMAIL ADDRESS" className="inp-2" />
+<input required onChange={onChangeHandler} name='street' value={formData.street} type="email" placeholder="STREET" className="inp-3" />
 <div className='div-5'>
-<input type="text" placeholder="CITY" className="inp-4" />
-<input type="text" placeholder="STATE" className="inp-5" />
+<input required onChange={onChangeHandler} name='City' value={formData.City} type="text" placeholder="CITY" className="inp-4" />
+<input required onChange={onChangeHandler} name='State' value={formData.State} type="text" placeholder="STATE" className="inp-5" />
 </div>
 <div className='div-6'>
-<input type="number" placeholder="ZIPCODE" className="inp-6" />
-<input type="text" placeholder="COUNTRY" className="inp-7" />
+<input required onChange={onChangeHandler} name='zipcode' value={formData.zipcode} type="number" placeholder="ZIPCODE" className="inp-6" />
+<input required onChange={onChangeHandler} name='country' value={formData.country} type="text" placeholder="COUNTRY" className="inp-7" />
 </div>
-<input type="number" placeholder="PHONE NUMBER" className="inp-6" />
+<input required  onChange={onChangeHandler} name='phone' value={formData.phone} type="number" placeholder="PHONE NUMBER" className="inp-6" />
 
 
       </div>
@@ -56,11 +78,11 @@ function PlaceOrder() {
 </div>
 </div>
 <div className='div-13'>
-<button onClick={()=>navigate('/orders')} className='bbbut'>PLACE ORDER</button>
+<button type='submit' className='bbbut'>PLACE ORDER</button>
 </div>
 </div>
       </div>
-    </div>
+    </from>
   )
 }
 
