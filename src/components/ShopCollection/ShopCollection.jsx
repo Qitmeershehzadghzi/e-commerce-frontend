e7234e984470c2822db65cc2f61893adcf27cfbc
Qitmeershehzadghzi <qitmeershehzad@gmail.com>
2025-09-28 15:26:30 +0500

    updated code

--- src/components/ShopCollection/ShopCollection.jsx
+++ src/components/ShopCollection/ShopCollection.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../../context/ShopContext';
 import Title from '../Title/Title';
-import ProductItem from '../productItem';
+import ProductItem from '../ProductItem/ProductItem';
 
 function ShopCollection() {
   const { products } = useContext(ShopContext);
